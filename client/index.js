836d2fb83178b08ed91c5175a48aec6adbddde80
Simon <simonyuanliu@gmail.com>
2018-11-05 14:03:57 -0800

    got css bundle working with webpack

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from './components/app.js';
-
+import css from './index.css';
 ReactDOM.render(<Menu/> , document.getElementById('app'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,30 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  // context: __dirname + '/client',
-  entry: path.join(__dirname,'/client/index.js'),
-  devtool: 'source-map',
-  mode:'development',
-  cache: true,
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env','@babel/preset-react']
-        },
-      },
-    ],
-  },
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js',
-  }
+    // context: __dirname + '/client',
+    entry: path.join(__dirname,'/client/index.js'),
+    devtool: 'source-map',
+    mode:'development',
+    cache: true,
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env','@babel/preset-react']
+                },
+            },
+        ],
+    },
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js',
+    }
 };
